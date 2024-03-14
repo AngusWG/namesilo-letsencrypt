cbe93368a53a48d25ec18d80b2b93f60c9a37899
AngusWG <z740713651@outlook.com>
2024-03-15 03:48:12 +0800

    refactor: add log print

--- config.py
+++ config.py
@@ -2,5 +2,7 @@ import os
 
 # Get your API Key from: https://www.namesilo.com/account_api.php
 apikey = os.environ["apikey"]
+print("apikey:", apikey)
 # Minutes to wait for DNS changes to complete.
-wait = os.environ["wait"]
+wait = int(os.environ.get("wait", "25"))
+print("wait:", wait)
--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,9 @@
 if [ -z "$(ls -A /etc/letsencrypt)" ]; then
     # /etc/letsencrypt folder is empty, indicating first run
     /bin/sh /app/namesilo-certbot.sh
+else
+    echo "/etc/letsencrypt folder exists."
 fi
 
+echo "supercronic /usr/local/etc/cronjob"
 supercronic /usr/local/etc/cronjob
